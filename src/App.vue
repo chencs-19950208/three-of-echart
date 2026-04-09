2cef13d2febdc19ca543a8b340561b2bd30ae680
cschen3 <cschen3@kxdigit.com>
2026-04-09 09:06:38 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <div class="header-content">
+          <h1 class="logo">Vue2 Template</h1>
+          <el-menu
+            :default-active="$route.path"
+            mode="horizontal"
+            router
+            background-color="#409EFF"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-menu-item index="/">首页</el-menu-item>
+            <el-menu-item index="/charts">ECharts</el-menu-item>
+            <el-menu-item index="/three">Three.js</el-menu-item>
+            <el-menu-item index="/about">关于</el-menu-item>
+          </el-menu>
+        </div>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', Arial, sans-serif;
+}
+
+.el-header {
+  background-color: #409EFF;
+  padding: 0 20px;
+}
+
+.header-content {
+  display: flex;
+  align-items: center;
+  height: 60px;
+}
+
+.logo {
+  color: #fff;
+  font-size: 20px;
+  margin-right: 40px;
+  white-space: nowrap;
+}
+
+.el-menu--horizontal {
+  border-bottom: none !important;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home.vue')
+  },
+  {
+    path: '/charts',
+    name: 'Charts',
+    component: () => import('@/views/Charts.vue')
+  },
+  {
+    path: '/three',
+    name: 'ThreeScene',
+    component: () => import('@/views/ThreeScene.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('@/views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
